9bc609d3c5a8182296c5554d44f64250ee801fd5
bg <bg@example.com>
2017-03-25 09:17:05 +0800

    WorkInTime 模板

--- WorkInTime.py
+++ WorkInTime.py
@@ -4,7 +4,7 @@ import time
 #timeBucket=[[时间起，时间止]，[时间点]*2]
 #从小到大排序，不支持跨日
 class WorkInTime():
-    def __init__(self, timeBucket, addTime=46):    #工作时间段和冗余时间
+    def __init__(self, timeBucket, relaxTime=60, addTime=4.6):    #工作时间段和冗余时间
         self.__time = timeBucket
         now = datetime.now()
         self.__timeType = [[time.mktime(time.strptime(str(now.year) + '-' + str(now.month) + '-' + str(now.day) +\
@@ -13,20 +13,34 @@ class WorkInTime():
 
         self.__timeType233 = [[i for i in timeB] for timeB in self.__time[:]
                          ]
-        self.addTime = addTime      #冗余时间
+        self.__addTime = addTime      #冗余时间
+        self.__relaxTime = relaxTime    #休息时间
+        self.__newday = True
+
+    def changeRelaxTime(self, relaxTime):
+        self.__relaxTime = relaxTime    #休息时间
+
+
     def relax(self):
         timeNow = time.time()
         timeBucket = self.__timeType
         if (timeNow > timeBucket[-1][1]):      #大于一天终止时间
             sleepTime = round(timeBucket[-1][0] - timeNow + 24 * 60 * 60, 0)
-            time.sleep(sleepTime+self.addTime)
+            time.sleep(sleepTime+self.__addTime)
+            self.__newday = True
         elif timeNow < timeBucket[0][0]:      #小于一天开始时间
-                sleepTime = round(timeBucket[0][0] - timeNow)
-                time.sleep(sleepTime + self.addTime)
+            sleepTime = round(timeBucket[0][0] - timeNow)
+            self.__newday = True
+            time.sleep(sleepTime + self.__addTime)
         else:
+            self.__newday = False
             for i in range(len(timeBucket)-1)[::-1]:
                 if (timeNow > timeBucket[i][1] and timeNow <= timeBucket[i+1][0]):
                     sleepTime = round(timeBucket[i+1][0]-timeNow)
-                    time.sleep(sleepTime+self.addTime)
+                    time.sleep(sleepTime+self.__addTime)
+        time.sleep(self.__relaxTime)
+
+    def isNewDay(self):
+        return self.__newday
 
 
